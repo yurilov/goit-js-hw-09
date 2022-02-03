60f46c7f2d9c5ba0526f0ff0aa269ec1b054b509
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-02-03 20:36:07 +0200

    added timer

---
+++ src/js/02-timer.js
@@ -0,0 +1,64 @@
+class Timer {
+  constructor({ targetDate }) {
+    this.targetDate = targetDate;
+    this.creatingFaceClock();
+    this.timerStart();
+  }
+
+  getDataForTimer() {
+    const time = this.targetDate - Date.now();
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((time % (1000 * 60)) / 1000);
+
+    return {
+      time,
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  }
+
+  creatingFaceClock() {
+    const faceClock = document.querySelector('.timer');
+
+    const refs = {
+      daysSpan: faceClock.querySelector(`[data-days]`),
+      hoursSpan: faceClock.querySelector(`[data-hours]`),
+      minsSpan: faceClock.querySelector(`[data-minutes]`),
+      secsSpan: faceClock.querySelector(`[data-seconds]`),
+    };
+
+    refs.daysSpan.textContent = String(this.getDataForTimer().days).padStart(2, '0');
+
+    refs.hoursSpan.textContent = String(this.getDataForTimer().hours).padStart(2, '0');
+
+    refs.minsSpan.textContent = String(this.getDataForTimer().minutes).padStart(2, '0');
+
+    refs.secsSpan.textContent = String(this.getDataForTimer().seconds).padStart(2, '0');
+  }
+
+  timerStart() {
+    const deadline = Date.parse(this.targetDate) <= Date.parse(new Date());
+    this.startForTimer = setInterval(() => {
+      if (deadline) {
+        clearInterval(this.startForTimer);
+        return;
+      }
+
+      this.creatingFaceClock();
+    }, 1000);
+  }
+}
+
+const dateTimePicker = document.querySelector('#datetime-picker');
+
+const startTimerRef = dateTimePicker.nextElementSibling;
+
+function startTimerHandler() {
+  const timer = new Timer({ targetDate: new Date(dateTimePicker.value) });
+}
+
+startTimerRef.addEventListener('click', startTimerHandler);
