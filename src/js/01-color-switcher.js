57d5c5b0f17db6d1cc744ab1172b74b878acf770
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-02-03 19:45:28 +0200

    completed 1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+
+startBtnRef.addEventListener('click', startBtnClickHandler);
+stopBtnRef.addEventListener('click', stopBtnClickHandler);
+
+function startBtnClickHandler() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    startBtnRef.setAttribute('disabled', 'true');
+  }, 1000);
+}
+
+function stopBtnClickHandler() {
+  clearInterval(timerId);
+  startBtnRef.removeAttribute('disabled');
+}
